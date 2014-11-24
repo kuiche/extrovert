33a8aa4a67bb41daf37ae57762e1d9b96edfbee8
kuiche <samtkeates@gmail.com>
2014-11-24 23:55:30 +0000

    Fixz

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-	db: null,//process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/extrovert',
+	db: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/extrovert',
 	assets: {
 		lib: {
 			css: [
